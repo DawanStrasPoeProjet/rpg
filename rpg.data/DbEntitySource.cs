6b7ebfaaf7de3eaf1dc75010e2cb2c307e0ad02d
Sylvain <sylvain.domange.dawan@outlook.fr>
2022-12-22 16:21:11 +0100

    BagItemIds string must be split

--- rpg.data/DbEntitySource.cs
+++ rpg.data/DbEntitySource.cs
@@ -13,10 +13,14 @@ public class DbEntitySource : IEntitySource
     }
 
     public IEntity Create(string id)
-    {        
+    {
         var dao = new Database.Dao.EntityDAO();
         Database.Model.Entity entity = dao.FindEntityById(id);
-        var e = new Entity(++_key, id, entity.Tags.Split(','), _itemSource , entity.DefaultEquippedItemId)
+
+        var tags = entity.Tags.Split(',');
+        var bagItemIds = entity.BagItemIds.Split(',');
+
+        var e = new Entity(++_key, id, tags, _itemSource , entity.DefaultEquippedItemId)
         {
             Initiative = entity.Initiative,
             Health = entity.Health,
@@ -31,8 +35,8 @@ public class DbEntitySource : IEntitySource
         };
         e.Rename(entity.Name);
         if (entity.BagItemIds != null)
-            foreach (var i in entity.BagItemIds)
-                e.Bag.AddItemById(i.ToString());
+            foreach (var i in bagItemIds)
+                e.Bag.AddItemById(i);
         if (entity.EquippedItemId != null)
             e.EquipNewItemById(entity.EquippedItemId);
         return e;
